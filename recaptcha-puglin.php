98f018a99273a11d9319ba0873d26d9e7ddd5391
Juliana <julianatsoc28@gmail.com>
2025-02-04 00:15:25 -0300

    feat: implement option on wp admin menu

---
+++ recaptcha-puglin.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+ * Puglin Name: reCAPTCHA para formulários
+ * Description: Adiciona o reCAPTCHA para todos os formulários HTML presentes no site
+ * Version: 1.0.0
+ * Author: julianatsoc
+ * Author URI: https://www.linkedin.com/in/julianatsoc/
+ */
+
+ 
